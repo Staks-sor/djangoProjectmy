122499ffd0d57920511ed649c513441ede6f7dc2
staks <stas.sor@gmail.com>
2022-09-20 21:30:58 +0300

    обновлений мало в основном изучение круд операций было через шел, вывод запись изменение, в модель добавил просмотра строки для тайтла, урл вернулся к изначальным записям по уроку. во вьюс так же

--- news/urls.py
+++ news/urls.py
@@ -2,16 +2,12 @@
 from django.urls import path
 
 from .views import *
-from .views import BlogListView
+
 
 urlpatterns = [
-    path('', BlogListView.as_view(), name='index'),
-    path('archive.html/', archive, name='archive'),
-    path('blog-details.html/', blog_details, name='blog_details'),
-    path('category.html/', category, name='category'),
-    path('contact.html/', contact, name='contact'),
-    # path('index.html/', index, name='index'),
-    path('css-single.html/', blog_single, name='css-single'),
+    path('', index, name='index'),
+    path('test/', test, name='test'),
+
 
 
 ]
--- news/views.py
+++ news/views.py
@@ -1,33 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import ListView
-from .models import News
 
 
-
-class BlogListView(ListView):
-    model = News
-    template_name = 'blog/index.html'
+def index(request):
+    return HttpResponse('Hello')
 
 
-
-
-
-def archive(request) -> render:
-    return render(request, template_name='blog/archive.html')
-
-
-def blog_details(request):
-    return render(request, template_name='blog/blog-details.html')
-
-
-def category(request) -> render:
-    return render(request, template_name='blog/category.html')
-
-
-def contact(request) -> render:
-    return render(request, template_name='blog/contact.html')
-
-
-def blog_single(request) -> render:
-    return render(request, template_name='blog/blog-single.html')
+def test(request):
+    return HttpResponse('<h1>Test page</h1>')
