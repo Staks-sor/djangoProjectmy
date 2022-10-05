730a4e26614cab6a770a60ae4e353c75a7bb02bd
staks_sor <stas.sor@gmail.com>
2022-10-05 15:42:17 +0300

    добавления фильтра в админку поиска

--- news/admin.py
+++ news/admin.py
@@ -1,14 +1,16 @@
 from django.contrib import admin
 
-from .models import News
+from .models import News, Category
 
 
 class NewsAdmin(admin.ModelAdmin):
     list_display = (
-        'id', 'title',
+        'id',
+        'title',
+        'category',
         'created_at',
         'update_at',
-        'is_published'
+        'is_published',
     )
     list_display_links = (
         'id', 'title'
@@ -17,6 +19,28 @@ class NewsAdmin(admin.ModelAdmin):
         'title',
         'content'
     )
+    list_editable = (
+        'is_published',
+    )
+    list_filter = (
+        'is_published',
+        'category'
+    )
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'title',
+    )
+    list_display_links = (
+        'id',
+        'title'
+    )
+    search_fields = (
+        'title',
+    )
 
 
 admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
