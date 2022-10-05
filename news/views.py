425f33b80cc447eb8402bfe79a19683905c443f5
staks_sor <stas.sor@gmail.com>
2022-10-05 10:48:43 +0300

    настройка админки добавление полей и ссылок на добавленные новости, поиск по новостям в админке

--- news/views.py
+++ news/views.py
@@ -2,9 +2,11 @@ from django.http import HttpResponse
 from django.shortcuts import render
 from .models import News
 
+
 def index(request):
-    news = News.objects.order_by('-created_at')
+    news = News.objects.all()
     return render(request, 'news/index.html', {'news': news, 'title': 'Список новостей'})
+
 
 def test(request):
     return HttpResponse('<h1>Test page</h1>')
