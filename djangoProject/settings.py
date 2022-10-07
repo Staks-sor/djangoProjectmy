11d9d596e1c876f15d90243cf74230d8fabde0c5
staks_sor <stas.sor@gmail.com>
2022-10-07 12:45:32 +0300

    обновление

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -58,8 +58,8 @@ ROOT_URLCONF = 'djangoProject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR,  'templates')]
-        ,
+        'DIRS': [os.path.join(BASE_DIR,  'templates')],
+
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,7 +67,9 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+
             ],
+
         },
     },
 ]
--- news/templatetags/news_tags.py
+++ news/templatetags/news_tags.py
@@ -5,6 +5,6 @@ from news.models import Category
 register = template.Library()
 
 
-@register.simple_tag()
+@register.simple_tag
 def get_categories():
     return Category.objects.all()
