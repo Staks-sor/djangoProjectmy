7a3b4400c94bceb0bb3f65b6058334826a7ce613
staks <stas.sor@gmail.com>
2022-10-25 19:24:18 +0300

    сделал авторизацию приветствие вошедшего и выход с возвратом на авторизацию делаю рассылку писем

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -152,3 +152,11 @@ INTERNAL_IPS = [
     '127.0.0.1',
 ]
 CSRF_TRUSTED_ORIGINS = ["https://5648-94-25-233-159.eu.ngrok.io"]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_USE_TLS = True
+EMAIL_PORT = 587
+EMAIL_HOST_USER = '@gmail.com'
+EMAIL_HOST_PASSWORD = ''
--- news/forms.py
+++ news/forms.py
@@ -1,8 +1,34 @@
 from django import forms
-from .models import News
-import re
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
+
+import re
+
 # from .models import Category
+from .models import News
+
+
+class UserLoginForm(AuthenticationForm):
+    username = forms.CharField(label='Имя пользователя',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'autocomlete': 'off'}))
+    password = forms.CharField(label='Пароль',
+                               widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+
+class UserRegisterForm(UserCreationForm):
+    username = forms.CharField(label='Имя пользователя', help_text='Имя не должно содержать оскорбления',
+                               widget=forms.TextInput(attrs={'class': 'form-control', 'autocomlete': 'off'}))
+
+    email = forms.EmailField(label='Почта', widget=forms.EmailInput(attrs={'class': 'form-control'}))
+    password1 = forms.CharField(label='Пароль', help_text='Пароль не может быть менее 8 символов',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+    password2 = forms.CharField(label='Подтверждение пароля',
+                                widget=forms.PasswordInput(attrs={'class': 'form-control'}))
+
+    class Meta:
+        model = User
+        fields = ('username', 'email', 'password1', 'password2')
 
 
 class NewsForm(forms.ModelForm):
